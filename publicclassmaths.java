7fe6aecbea21eabdd5c1925784be7f7c79f54a75
RohanDixit52 <101317368+RohanDixit52@users.noreply.github.com>
2022-03-11 11:02:30 +0530

    Add files via upload

---
+++ publicclassmaths.java
@@ -0,0 +1,17 @@
+class maths
+	{
+	public static void main(String[]args)
+	{
+		int n1= 5, n2=4, sum, multiply, divide, sub;
+
+		sum = n1 + n2;
+		multiply = n1*n2;
+		divide = n1/n2;
+		sub = n1 - n2;
+
+		System.out.println("sum = " + sum);
+		System.out.println("multiply = " + multiply);
+		System.out.println("divide = " + divide);
+		System.out.println("sub = " + sub);
+	} 
+	}
